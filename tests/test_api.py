c78e9f32809bcf954c3e2c1f5ee9954bc55f11f7
Dave Bush <github@david-bush.co.uk>
2017-02-14 14:03:55 +1300

    Fixed errors bug, updated docstrings and added API_KEY to constructor.

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,10 +6,12 @@ from smtp2go.exceptions import SMTP2GoAPIKeyException
 from smtp2go.settings import API_ROOT, ENDPOINT_SEND
 from smtp2go.core import SMTP2Go
 
+TEST_API_KEY = 'testapikey'
+
 
 @responses.activate
 def test_successful_endpoint_send(monkeypatch):
-    monkeypatch.setenv('SMTP2GO_API_KEY', 'testkey')
+    monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
     # Mock out API Endpoint:
     successful_response = {
         "request_id": "aa253464-0bd0-467a-b24b-6159dcd7be60",
@@ -33,19 +35,24 @@ def test_successful_endpoint_send(monkeypatch):
     assert resp.success is True
     assert resp.json == successful_response
     assert resp.status_code == http_return_code
-    assert resp.errors == successful_response.get('')
+    assert not resp.errors
+    assert resp.errors == successful_response.get('data').get('failures')
 
 
 @responses.activate
 def test_failed_endpoint_send(monkeypatch):
-    monkeypatch.setenv('SMTP2GO_API_KEY', 'testkey')
+    monkeypatch.setenv('SMTP2GO_API_KEY', TEST_API_KEY)
     # Mock out API Endpoint:
     failed_response = {
         "request_id": "aa253464-0bd0-467a-b24b-6159dcd7be60",
         "data": {
             "succeeded": 0,
             "failed": 1,
-            "failures": ['Uh-oh spagettios!']
+            "failures": [
+                'The API Key passed was not in the correct format, Please '
+                'check the key is correct and try again, The full API key can '
+                'be found in the API Keys section in the admin console.'
+            ]
         }
     }
     http_return_code = 400
@@ -62,10 +69,16 @@ def test_failed_endpoint_send(monkeypatch):
     assert resp.success is False
     assert resp.status_code == http_return_code
     assert resp.json == failed_response
-    assert resp.errors == failed_response.get('errors')
+    assert not resp.errors == []
+    assert resp.errors == failed_response.get('data').get('failures')
 
 
 def test_no_environment_variable_raises_api_exception():
     assert os.getenv('SMTP2GO_API_KEY') is None
     with pytest.raises(SMTP2GoAPIKeyException):
-        SMTP2Go()
+        SMTP2Go()  # Called without api_key in constructor
+
+
+def test_api_key_variable_in_constructor():
+    assert os.getenv('SMTP2GO_API_KEY') is None
+    SMTP2Go(api_key=TEST_API_KEY)
