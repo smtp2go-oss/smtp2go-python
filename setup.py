896e1b46f7ba6a0c73c6001271ff7056239aa7f1
Dave Bush <github@david-bush.co.uk>
2017-02-21 14:33:44 +1300

    Bumped version, added classifiers and updated source control URL.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='smtp2go',
-      version='0.0.1',
-      description='Library for interacting with the SMTP2Go API.',
-      url='https://github.com/smtp2go/smtp2go.api-python',
+      version='0.0.2',
+      description='Library for interfacing with the smtp2go API.',
+      url='https://github.com/smtp2go-oss/smtp2go-python',
       author='SMTP2Go',
       author_email='devs@smtp2go.com',
       license='MIT',
@@ -12,10 +12,12 @@ setup(name='smtp2go',
           'requests'
       ],
       classifiers=[
-          "Development Status :: 3 - Alpha",
+          "Development Status :: 5 - Production",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python",
-          "Topic :: Software Development :: Libraries"
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.0",
+          "Topic :: Communications :: Email"
+          "Topic :: Software Development :: Libraries :: Python Modules"
       ],
       zip_safe=False)
